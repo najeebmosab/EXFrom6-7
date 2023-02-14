89d8a0847664068dba9ecb95296b3248b225227c
najeebmosab <73877639+najeebmosab@users.noreply.github.com>
2023-02-14 13:13:09 +0200

    add EX6.3

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import './App.css'
 import Button from "./Components/ClickPage"
 import Box from "./Components/boxComponent"
 function App() {
+
   let [num, setNumber] = useState(0);
   const [flag, setFlage] = useState(true);
+
+  let [n, setN] = useState(0);
+
   const Handler = () => {
     debugger;
     console.log(flag);
     setFlage(!flag);
     console.log(flag);
   }
+
   return (
     <div>
       <Button val={num}></Button>
@@ -22,8 +27,13 @@ function App() {
         <button onClick={Handler}>Show</button>
         <Box myFlag={!flag}></Box>
       </div>
+      <div>
+        <button onClick={function(){if(n>=10)return n = 10 ;setN(n += 1); }}>increment</button>
+        <label htmlFor="" className={n>0?'greenText': n<0 ? 'redText': n==0 ? 'blackText':''}>{n}</label>
+        <button onClick={function(){if(n <= -10)return n = -10;setN(n-=1)}}>decrement</button>
+      </div>
     </div>)
-  
+
 }
 
 export default App
